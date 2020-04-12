9aa422d338e5c2a289c920fedece460580a49f90
Allan Goncalves <allangoncalves@outlook.com>
2020-04-12 17:04:33 -0300

    add a custom pagination

--- backend/tarolok/settings/base.py
+++ backend/tarolok/settings/base.py
@@ -85,8 +85,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "tarolok.wsgi.application"
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 30,
+    'DEFAULT_PAGINATION_CLASS': 'api.pagination.CustomPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
